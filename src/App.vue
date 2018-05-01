ece8b45e8db60b9bdb2e7f16666dc9a8d1435a97
Ben Casalino <bencasalino@gmail.com>
2018-05-01 15:50:43 -0600

    dkslfj

--- src/App.vue
+++ src/App.vue
@@ -47,7 +47,8 @@ export default {
 		Champions,
 		Shutouts,
 		Matches,
-Section, Footer
+		Section,
+		Footer
   }
 }
 </script>
--- src/components/API.vue
+++ src/components/API.vue
@@ -1,28 +1,55 @@
 <template>
 <div class="api-wrapper">
 <p class="label"> API</p>
-
-
-<p>
+<br>
+<div class="api-split">
+<div>
+  <p>What is this?</p>
   <br>
-  <p>/MATCHES</p>
-  <a href="https://rmcup.herokuapp.com/matches" target="_blank">  https://rmcup.herokuapp.com/matches</a>
-  </p>
-    <br>
-  <p>/CHAMPIONS</p>
-  <a href="https://rmcup.herokuapp.com/champions" target="_blank">  https://rmcup.herokuapp.com/champions</a>
-  </p>
-    <br>
-  <p>/GOALS</p>
-  <a href="https://rmcup.herokuapp.com/goals" target="_blank">  https://rmcup.herokuapp.com/goals</a>
-  </p>
-    <br>
-  <p>/SHUTOUTS</p>
-  <a href="https://rmcup.herokuapp.com/shutouts" target="_blank">  https://rmcup.herokuapp.com/shutouts</a>
-  </p>
+  <p>This is a full RESTful API linked to an extensive database detailing everything about the Rocky Mountain Cup series.</p>
+  <br>
+  <div class="api-routes">
+<p class="supa-bold">ex: https://rmcup.herokuapp.com/{endpoint}</p>
+<br>
+<p class="api-label">/MATCHES</p>
+<a href="https://rmcup.herokuapp.com/matches" target="_blank">  https://rmcup.herokuapp.com/matches</a>
+<br>
+<br>
+<p class="api-label">/CHAMPIONS</p>
+<a href="https://rmcup.herokuapp.com/champions" target="_blank">  https://rmcup.herokuapp.com/champions</a>
+<br>
+<br>
+<p class="api-label">/GOALS</p>
+<a href="https://rmcup.herokuapp.com/goals" target="_blank">  https://rmcup.herokuapp.com/goals</a>
+<br>
+<br>
+<p class="api-label">/SHUTOUTS</p>
+<a href="https://rmcup.herokuapp.com/shutouts" target="_blank">  https://rmcup.herokuapp.com/shutouts</a>
+</div>
+</div>
+<br>
+<div>
+  <p>Where do I start?</p>
+  <br>
+  <p>Calling any API endpoint without a resource ID or name will return a paginated list of available resources for that API. </p>
+  <br>
+    <div class="code-ex">
+      <code>
+        {
+          <br>
+"id": 19,          <br>
+"date": "September 25, 2010",          <br>
+"home": "RSL",          <br>
+"away": "COL",          <br>
+"score": "1-1",          <br>
+"series": "COL 8-6-5"          <br>
+}       <br>
+      </code>
+    </div>
+  </div>
+</div>
 
-
-
+  <!-- wrapper -->
   </div>
 </template>
 
@@ -61,4 +88,33 @@ a {
   text-decoration: none;
 }
 
+.api-split {
+display: flex;
+justify-content: space-between;
+}
+
+.code-ex {
+    font-weight: 400;
+  font-family: 'Kreon', serif;
+  font-size: 20px;
+  padding: 33px;
+  background-color: #333;
+  color: #aaa;
+  max-width: 200px;  box-shadow: 0px 0px 20px rgba(0,0,0,0.10),
+0px 10px 20px rgba(0,0,0,0.05),
+0px 20px 20px rgba(0,0,0,0.05),
+0px 30px 20px rgba(0,0,0,0.05);
+border-bottom: 4px #9B9487 solid;
+}
+
+.supa-bold {
+    background-color: #333;
+  color: #aaa;
+  border-bottom: 4px #9B9487 solid;
+  font-weight: 900;
+  font-style: italic;
+  /* background-color: #aaa; */
+  max-width: 320px;
+  padding: 5px;
+}
 </style>
